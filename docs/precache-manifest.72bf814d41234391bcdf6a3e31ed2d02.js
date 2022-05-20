03e4596924aa28ef5ece7ee4a87f4da2a84a895f
erickcr91496 <eavasconeze@utn.edu.ec>
2022-05-19 22:37:51 -0500

    docs agregado

---
+++ docs/precache-manifest.72bf814d41234391bcdf6a3e31ed2d02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2884f9ee0fb6cd3294a84c02a77eddea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81a8fc5935ecc30f99a4",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "f0118576ccb96158c618",
+    "url": "/static/js/2.0bc91250.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.0bc91250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a8fc5935ecc30f99a4",
+    "url": "/static/js/main.7d4b33bb.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
